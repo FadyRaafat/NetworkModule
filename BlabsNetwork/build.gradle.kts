87e4a9e4188e0b67c9ee9f778576ff20f9ebd08c
Amr Hany <amr.eldeeb@btech.com>
2024-03-24 21:41:00 +0200

    feat: add chucker

--- BlabsNetwork/build.gradle.kts
+++ BlabsNetwork/build.gradle.kts
@@ -19,7 +19,8 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
             )
         }
     }
@@ -33,7 +34,8 @@ android {
 }
 
 dependencies {
-
+    debugImplementation("com.github.chuckerteam.chucker:library:4.0.0")
+    releaseImplementation("com.github.chuckerteam.chucker:library-no-op:4.0.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -55,6 +57,4 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("com.mcxiaoke.volley:library:1.0.19")
-
-
 }
